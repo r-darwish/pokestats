e26793274e6d869289d54207dc91e09bacc56f02
Roey Darwish Dror <roey.ghost@gmail.com>
2020-07-25 14:38:43 +0300

    Initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>Insert a pokemon name in English or Hebrew:</p>
+    <br />
+    <input v-model="pokemonFilter" placeholder="Pokemon" />
+    <ul v-if="shouldSearch">
+      <li
+        v-for="pokemon in filteredData"
+        :key="pokemon"
+      >{{pokemon.hebrewName}} / {{pokemon.englishName}}</li>
+    </ul>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+interface Pokemon {
+  hebrewName: string;
+  englishName: string;
+}
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    const pokemonData: Pokemon[] = [];
+
+    return { pokemonFilter: "", pokemons: pokemonData };
+  },
+  computed: {
+    shouldSearch(): boolean {
+      return this.pokemonFilter.length > 3;
+    },
+    filteredData(): Pokemon[] {
+      return this.pokemons.filter(
+        pokemon =>
+          pokemon.hebrewName.includes(this.pokemonFilter) ||
+          pokemon.englishName.includes(this.pokemonFilter)
+      );
+    }
   }
 };
 </script>
