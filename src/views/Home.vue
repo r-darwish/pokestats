a12f6574e0795ca15785d841de507fd6f8a8d48a
Roey Darwish Dror <roey.ghost@gmail.com>
2020-07-25 23:14:37 +0300

    Very basic interface

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <p>Insert a pokemon name in English or Hebrew:</p>
     <br />
-    <input v-model="pokemonFilter" placeholder="Pokemon" />
+    <input v-model="pokemonFilter" placeholder="Pokemon" autofocus />
     <div v-if="shouldSearch">
       <div v-for="pokemon in filteredData" :key="pokemon">
         <a :href="`https://www.pocketmonsters.co.il/?p=${pokemon.linkId}`">
@@ -10,7 +10,20 @@
             :src="`https://www.pocketmonsters.co.il/wp-content/uploads/2020/06/${pokemon.imageId}-1.png`"
           />
         </a>
+        <br />
         {{pokemon.hebrewName}} / {{pokemon.englishName}}
+        <p>
+          <strong>Major Weaknesses:</strong>
+          {{pokemon.majorWeaknessess}}
+        </p>
+        <p>
+          <strong>Weaknesses:</strong>
+          {{pokemon.weaknessess}}
+        </p>
+        <p>
+          <strong>Resistancess:</strong>
+          {{pokemon.resistancess}}
+        </p>
       </div>
     </div>
   </div>
@@ -19,11 +32,18 @@
 <script lang="ts">
 import { Component, Vue, Watch } from "vue-property-decorator";
 
+enum PokemonType {
+  Fire = "Fire"
+}
+
 interface Pokemon {
   hebrewName: string;
   englishName: string;
   linkId: number;
   imageId: number;
+  weaknessess: PokemonType[];
+  majorWeaknessess: PokemonType[];
+  resistancess: PokemonType[];
 }
 
 @Component({})
@@ -33,13 +53,16 @@ export default class Home extends Vue {
       hebrewName: "נגביר",
       englishName: "Negeveer",
       imageId: 650,
-      linkId: 87267
+      linkId: 87267,
+      majorWeaknessess: [PokemonType.Fire],
+      weaknessess: [PokemonType.Fire],
+      resistancess: [PokemonType.Fire]
     }
   ];
   private pokemonFilter = "";
 
   get shouldSearch(): boolean {
-    return this.pokemonFilter.length > 3;
+    return this.pokemonFilter.length >= 3;
   }
   get filteredData(): Pokemon[] {
     return this.pokemonData.filter(
