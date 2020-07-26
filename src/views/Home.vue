85ba65e41a910d5ecaabb2a3e8f5b0ea02262c80
Roey Darwish Dror <roey.ghost@gmail.com>
2020-07-26 22:13:18 +0300

    Better design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,28 +4,54 @@
     <br />
     <input v-model="pokemonFilter" placeholder="Pokemon" autofocus />
     <div v-if="shouldSearch">
-      <div v-for="pokemon in filteredData" :key="pokemon">
+      <div v-for="pokemon in filteredData" :key="pokemon" class="pokemon">
+        <h3>{{pokemon.hebrewName}} / {{pokemon.englishName}}</h3>
         <a :href="`https://www.pocketmonsters.co.il/?p=${pokemon.linkId}`" target="_blank">
           <img :src="pokemon.imageId" />
         </a>
-        <br />
-        {{pokemon.hebrewName}} / {{pokemon.englishName}}
-        <p>
-          <strong>Major Weaknesses:</strong>
-          {{pokemon.majorWeaknesses}}
-        </p>
-        <p>
-          <strong>Weaknesses:</strong>
-          {{pokemon.weaknesses}}
-        </p>
-        <p>
-          <strong>Resistancess:</strong>
-          {{pokemon.resistancess}}
-        </p>
+        <div class="container" v-if="pokemon.majorWeaknesses.length">
+          Major Weaknesses:
+          <img
+            v-for="t in pokemon.majorWeaknesses"
+            :key="t"
+            :src="t"
+            class="pokemon-type"
+          />
+        </div>
+        <div class="container">
+          Weaknesses:
+          <img v-for="t in pokemon.weaknesses" :key="t" :src="t" class="pokemon-type" />
+        </div>
+        <div class="container">
+          Resistancess:
+          <img
+            v-for="t in pokemon.resistancess"
+            :key="t"
+            :src="t"
+            class="pokemon-type"
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5em;
+  justify-content: center;
+}
+
+.pokemon-type {
+  margin-left: 0.2em;
+}
+
+.pokemon {
+  margin-bottom: 5em;
+}
+</style>
 
 <script lang="ts">
 import { Component, Vue, Watch } from "vue-property-decorator";
